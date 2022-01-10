41e2e458141dfe0824a008b6a460f02ad59a31d1
jordan-liubenov <jordan.liubenov62@gmail.com>
2022-01-11 01:41:12 +0200

    added projects to Projects section (needs more work)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let defaultEmailState = true;
 
 function displayEmail(){ //show or hide email on click
     if(defaultEmailState){
-        email.innerHTML = '(Email: jordan.liubenov62@gmail.com)'
+        email.innerHTML = '*(Email: jordan.liubenov62@gmail.com)*'
         defaultEmailState = false;
     } else {
         email.innerHTML = '';
