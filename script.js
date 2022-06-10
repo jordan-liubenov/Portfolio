b238e3c58d370a26259e0ccb9b9a215be5469738
jordan-liubenov <jordan.liubenov62@gmail.com>
2022-06-10 20:58:50 +0300

    stylized scrollbar and added project links

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const email = document.querySelector('#email');
 let defaultEmailState = true;
 
-function displayEmail(){ //show or hide email on click
-    if(defaultEmailState){
-        email.innerHTML = '*(Email: jordan.liubenov62@gmail.com)*'
+function displayEmail() {
+    if (defaultEmailState) {
+        email.textContent = '*(Email: jordan.liubenov62@gmail.com)*'
         defaultEmailState = false;
     } else {
-        email.innerHTML = '';
+        email.textContent = '';
         defaultEmailState = true;
     }
+}
+
+window.addEventListener("load", main);
+
+function main() {
+    openPassGen();
+    openWeatherApp();
+    openTimerApp();
+}
+
+function openPassGen() {
+    let image = document.querySelector(".genIMG");
+    image.addEventListener("click", () => {
+        window.open("https://jordan-liubenov.github.io/Password-Generator/")
+    });
+}
+function openWeatherApp() {
+    let image = document.querySelector(".weatherIMG");
+    image.addEventListener("click", () => {
+        window.open("https://jordan-liubenov.github.io/Weather-App/")
+    });
+}
+function openTimerApp() {
+    let image = document.querySelector(".timerIMG");
+    image.addEventListener("click", () => {
+        window.open("https://jordan-liubenov.github.io/Focus-Timer/")
+    });
 }
